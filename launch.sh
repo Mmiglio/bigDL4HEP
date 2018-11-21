52c4fdbcf7f0b34aeae3b769506654ee78e0fe71
Mmiglio <matteo.migliorini95@gmail.com>
2018-11-21 15:32:16 +0100

    Tested submition script and SparkCOntext creation

---
+++ launch.sh
@@ -0,0 +1,14 @@
+export SPARK_HOME=/afs/cern.ch/work/m/migliori/public/spark-2.3.1
+export BIGDL_HOME=/afs/cern.ch/work/m/migliori/public/BDLtest/bigDL
+
+export PYSPARK_PYTHON=/afs/cern.ch/work/m/migliori/public/anaconda2/bin/python
+
+${BIGDL_HOME}/bin/spark-submit-with-bigdl.sh \
+--master yarn \
+--conf spark.driver.memory=5G \
+--conf spark.driver.cores=4 \
+--conf spark.executor.instances=2 \
+--conf spark.executor.cores=4 \
+--conf spark.executor.memory=14G \
+HLFclassifier.py \
+--dataset /afs/cern.ch/work/m/migliori/test20k.parquet
--- scripts/HLFclassifier.py
+++ scripts/HLFclassifier.py
@@ -1,2 +1,25 @@
+from bigdl.util.common import *
+
+import argparse
+
 if __name__ == "__main__":
-    pass
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, nargs=1)
+
+    args = parser.parse_args()
+
+    ## Create SparkContext and SparkSession
+    sc = SparkContext(
+        appName="HLFclassifier",
+        conf=create_spark_conf())
+    spark = SQLContext(sc)
+
+    ## Init bigDL and show logs
+    sc.setLogLevel("ERROR")
+    show_bigdl_info_logs()
+    init_engine()
+    
+
+
+    
